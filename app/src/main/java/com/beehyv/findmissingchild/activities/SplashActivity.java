6e43a9b7062763496b6b7027cd3de3786f2cc213
challa913 <challa.rao@beehyv.com>
2016-04-25 18:42:43 +0530

    camera functionality and image display

--- app/src/main/java/com/beehyv/findmissingchild/activities/SplashActivity.java
+++ app/src/main/java/com/beehyv/findmissingchild/activities/SplashActivity.java
@@ -1,19 +1,22 @@
 package com.beehyv.findmissingchild.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 
-import com.beehyv.findmissingchild.R;
+import com.beehyv.findmissingchild.adapters.ImageAdapter;
 
+/**
+ * Created by rishan on 25/4/16.
+ */
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
 
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
-
 }
--- app/src/main/java/com/beehyv/findmissingchild/adapters/ImageAdapter.java
+++ app/src/main/java/com/beehyv/findmissingchild/adapters/ImageAdapter.java
@@ -1,5 +1,7 @@
 package com.beehyv.findmissingchild.adapters;
 
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -8,6 +10,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.beehyv.findmissingchild.R;
+import com.beehyv.findmissingchild.activities.MainActivity;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,33 +20,68 @@ import java.util.List;
  */
 public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.CustomImageViewHolder> {
 
-    List<Bitmap> clickedImages=new ArrayList<>();
+    private List<Bitmap> clickedImages=new ArrayList<>();
+    private interactionInterface listener;
+    public final static int IMAGE_DISPLAY_VIEWTYPE=0;
+    public final static int ADD_IMAGE_VIEWTYPE=1;
 
-    public ImageAdapter(ArrayList<Bitmap> clickedImages){
+    public ImageAdapter(ArrayList<Bitmap> clickedImages, interactionInterface listener){
         this.clickedImages=clickedImages;
+        this.listener = listener;
     }
 
     @Override
     public CustomImageViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view=LayoutInflater.from(parent.getContext()).inflate(R.layout.image_view, null);
-        return new CustomImageViewHolder(view);
+        View view;
+        if(viewType==ADD_IMAGE_VIEWTYPE)
+            view = LayoutInflater.from(parent.getContext()).inflate(R.layout.add_image, null);
+        else
+            view = LayoutInflater.from(parent.getContext()).inflate(R.layout.image_view, null);
+        return new CustomImageViewHolder(view,viewType);
+
     }
 
     @Override
     public void onBindViewHolder(CustomImageViewHolder holder, int position) {
-        holder.singleImage.setImageBitmap(clickedImages.get(position));
+        if(getItemViewType(position)!=ADD_IMAGE_VIEWTYPE)
+            holder.singleImage.setImageBitmap(clickedImages.get(position));
+        else {
+            holder.takePic.setImageResource(R.drawable.take_pic);
+
+            holder.takePic.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    listener.loadCamera();
+                }
+            });
+        }
     }
 
     @Override
     public int getItemCount() {
-        return clickedImages.size();
+        return clickedImages.size()+1;
+    }
+    @Override
+    public int getItemViewType(int position) {
+        if(position==getItemCount()-1)
+            return ADD_IMAGE_VIEWTYPE;
+        else
+            return IMAGE_DISPLAY_VIEWTYPE;
     }
 
     public class CustomImageViewHolder extends RecyclerView.ViewHolder{
         ImageView singleImage;
-        public CustomImageViewHolder(View itemView) {
+        ImageView takePic;
+        public CustomImageViewHolder(View itemView,int viewType) {
             super(itemView);
             this.singleImage=(ImageView)itemView.findViewById(R.id.singleImage);
+            if(viewType==ADD_IMAGE_VIEWTYPE)
+                this.takePic=(ImageView)itemView.findViewById(R.id.take_pic);
         }
+    }
+
+
+    public interface interactionInterface {
+        void loadCamera();
     }
 }
